1d34556981d898f5a4829fed69302777d9c69d98
LeeKiWoo <pain0121@naver.com>
2022-04-17 13:12:14 +0900

    localStorage

--- 2_Chrome with VanillaJs/js/app.js
+++ 2_Chrome with VanillaJs/js/app.js
@@ -1,78 +1,31 @@
-const amIFAt = true,
-  amIbrain = null; // boolean, null(nothing in this)
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector(".login-form input");
+const loginButton = document.querySelector(".login-form button");
+const textInput = document.querySelector("#greeting");
+const HIDEN_CLASSNAME = "hidden";
 
-let something; // undefine (variable is exist, but it isn't got a value)
+greetingUser = () => {
+  const userName = localStorage.getItem("username");
 
-// Array
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-const nonsense = [1, 2, "hello", false, null, true, undefined];
-
-daysOfWeek[1];
-daysOfWeek.push("end");
-
-// Object
-const player = {
-  name: "kais",
-  points: 10,
-  fat: true,
+  loginForm.classList.add(HIDEN_CLASSNAME);
+  textInput.innerHTML = `Hello ${userName}`;
+  textInput.classList.remove(HIDEN_CLASSNAME);
 };
 
-player.fat = false;
-player.lastName = "potato"; // object 끝에 property 추가
-player.points = player.points + 15;
-console.log(player.points);
+onsubmit = (toto) => {
+  toto.preventDefault();
+  // submit의 기본 동작을 막음
 
-// Function
-function helloFriends(name, age) {
-  console.log(`hello ${name}!!! your age is ${age}`);
+  const userName = loginInput.value;
+
+  localStorage.setItem("username", userName);
+  // 로컬 저장소에 username 저장
+
+  greetingUser();
+};
+
+if (localStorage.getItem("username") !== null) {
+  greetingUser();
 }
 
-helloFriends("kai", Math.floor(20.1));
-helloFriends("adf", 21);
-helloFriends("meorl", 24);
-helloFriends("rachel", 23);
-helloFriends("TOm", 21);
-
-const listener = {
-  name: "nico",
-  sayHello: function () {
-    console.log("hello!");
-  },
-};
-
-listener.sayHello();
-
-// Recap
-
-const calculator = {
-  add: function (a, b) {
-    let result = a + b;
-    return result;
-  },
-  minus: function (a, b) {
-    let result = a - b;
-    return result;
-  },
-  divide: function (a, b) {
-    let result = a / b;
-    return result;
-  },
-  powerOff: function (a, b) {
-    let result = a ** b;
-    return result;
-  },
-  times: function (a, b) {
-    let result = a * b;
-    return result;
-  },
-};
-
-console.log(calculator.add(2, 5));
-
-// conditionals (조건문)
-const age = "22";
-console.log(typeof player.fat);
-
-age = parseInt(age);
-
-console.log(isNaN(age)); // 매개변수가 Nan 일경우 true 반환
+loginForm.addEventListener("submit", onsubmit);
